52947f7e8039fa5e2bb5f1c157ca4b0e7957a238
CranialHeartache <hdsnappendragon@hotmail.com>
2022-09-24 22:26:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ async def on_guild_channel_pins_update(channel, last_pin):
 
 
 # TODO Replace TOKEN with the token from discord developer portal 
-client.run('MTAyMzM5ODEzMDM5OTU4ODM1Mg.GRVxfW.F1l-L5RToEGjLWX2ccMHc3nkOK8diw5HM-xf-4')
+client.run(process.env.token)
 
 # TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
 client.run(os.environ.get('TOKEN'))
